4674c0788d3528e3cd2155d782a788c797e594f4
Eric Paul <eric.1334@icloud.com>
2026-02-03 23:58:14 -0600

    Add placeholder for inferType function

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open System_e_kernel
+open Term
+
+let inferType (env : environment) (localCtx : localcontext) (t : term) : term =
+  match t with
+  | Const name -> failwith "infer type of const"
+  | Fvar name -> failwith "infer type of a free variable"
+  | Bvar idx -> failwith "infer type of a bound variable"
+  | App (func, arg) -> failwith "infer type of function application"
+  | Lam (domainType, body) -> failwith "infer type of a function"
+  | Forall (domainType, returnType) -> failwith "infer type of a forall"
+  | Sort level -> failwith "infer type of a sort"
+
+let () = print_endline "test"
+
---
+++ lib/term.ml
@@ -0,0 +1,14 @@
+type term =
+    | Const of string
+    | Bvar of int
+    | Fvar of string
+    | Lam of term * term
+    | Forall of term * term
+    | App of term * term
+    | Sort of int
+
+(* Maps constants to their types *)
+type environment = (string, term) Hashtbl.t
+
+(* Maps free variables to their types *)
+type localcontext = (string, term) Hashtbl.t
