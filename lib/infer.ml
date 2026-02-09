6f74e1631d6aeec06dda9b1d78e55ba058c3af87
char3210 <45241413+char3210@users.noreply.github.com>
2026-02-09 17:44:51 -0600

    add comments

--- lib/infer.ml
+++ lib/infer.ml
@@ -18,6 +18,11 @@ let rec subst_bvar (t: term) (bvar_idx: int) (bvar_val: term) : term =
         let arg_subst = subst_bvar arg bvar_idx bvar_val in
         App (func_subst, arg_subst)
 
+(* In the term t, converts a free variable fvar_name to a bound variable with index 
+bvar_idx relative to the top level. Replaces all `FVar fvar_name` references with the
+appropriate `Bvar idx` reference. This will return a term that by itself will have 
+out-of-bounds Bvar references, so it should be placed in an appropriate Lam/Forall 
+to be valid. *)
 let rec rebind_bvar (t: term) (bvar_idx: int) (fvar_name: string) : term = 
   match t with
     | Const _ | Sort _  | Bvar _ -> t
@@ -69,17 +74,21 @@ let rec inferType (env : environment) (localCtx : localcontext) (t : term) : ter
   )
   | Lam (domainType, body) -> (
       let new_fvar_name = gen_new_fvar_name () in
+      (* add mapping new_fvar_name -> domainType to localCtx in recursive call *)
+      (* this is fine because domainType won't have any unresolved BVars *)
       let newLocalCtx = 
         let t = Hashtbl.copy localCtx in
         Hashtbl.replace t new_fvar_name domainType;
         t
       in
+      (* replace bound variable with new free variable *)
       let substed_term = subst_bvar body 0 (Fvar new_fvar_name) in
       let ret_type = inferType env newLocalCtx substed_term in
+      (* replace the free variable with bound variable *)
       let rebound_type = rebind_bvar ret_type 0 new_fvar_name in
       Forall (domainType, rebound_type)
     )
-  | Forall (_domainType, _returnType) -> Sort 0
+  | Forall (_domainType, _returnType) -> Sort 0 (* todo: soundness, consider the universes of domainType, returnType. cap at Sort 2? *)
   | Sort level -> Sort (level + 1)
 
 let mk_axioms_env () =
