ac5b1d521ce37f8ed286a3d04257d1aac6c9b686
PalindromeLeung <isprime67@gmail.com>
2026-03-29 16:41:40 -0500

    add reflexivity tactic

---
+++ elab/tactics.ml
@@ -0,0 +1,63 @@
+open Term
+open Types
+open Proofstate 
+
+type tactic_result =
+  | Success of proof_state
+  | Failure of string
+
+let succeed st  = Success st
+let fail    msg = Failure msg 
+
+let catch_elab (ectx : ctx) (f : unit -> tactic_result) : tactic_result =
+  try f ()
+  with Error.ElabError info -> Failure (Error.pp_exn ectx info)
+
+
+let beta_nf (e : ctx) (tm : term) : term = Reduce.reduce e tm
+
+
+let rec def_eq (e : ctx) (t1 : term) (t2 : term) : bool =
+  let t1 = beta_nf e t1 in
+  let t2 = beta_nf e t2 in
+  match t1.inner, t2.inner with
+  | Sort i,    Sort j    -> i = j
+  | Bvar i,    Bvar j    -> i = j
+  | Name s,    Name t    -> s = t
+  | Hole i,    Hole j    -> i = j
+  | App (f1, x1), App (f2, x2) ->
+      def_eq e f1 f2 && def_eq e x1 x2
+  | Fun (_, bid1, ty1, b1), Fun (_, bid2, ty2, b2) ->
+      def_eq e ty1 ty2 &&
+      def_eq e b1 (Reduce.subst e b2 (Bvar bid2) (Bvar bid1))
+  | Arrow (_, bid1, ty1, r1), Arrow (_, bid2, ty2, r2) ->
+      def_eq e ty1 ty2 &&
+      def_eq e r1 (Reduce.subst e r2 (Bvar bid2) (Bvar bid1))
+  | _ -> false
+
+let goal_stack (g : goal) : int list = 
+	List.map (fun h -> h.hyp_bid) g.ctx 
+
+(*[Eq A lhs rhs] when [lhs] and [rhs] are definitionally equal, proof term is [@refl A lhs].*)
+let reflexivity (st : proof_state) : tactic_result =
+  match current_goal st with
+  | None -> fail "No goals remaining."
+  | Some g ->
+      let ty = beta_nf st.elab_ctx g.goal_type in
+      (match ty.inner with
+       | App ({ inner = App ({ inner = App ({ inner = Name "Eq"; _ }, _a); _ }, lhs); _ }, rhs) ->
+           if def_eq st.elab_ctx lhs rhs then
+             let proof = mk_app (mk_app (mk_name "refl") _a) lhs in
+             let st = assign_meta g.goal_id proof st in
+             let st = close_goal  g.goal_id st in
+             succeed st
+           else
+             fail (Printf.sprintf
+               "reflexivity: lhs '%s' and rhs '%s' are not definitionally equal."
+               (pp_term st.elab_ctx lhs) (pp_term st.elab_ctx rhs))
+               (* need to map to existing error categories*)
+       | _ ->
+           fail (Printf.sprintf
+             "reflexivity: goal '%s' is not of the form 'Eq A t t'."
+             (pp_term st.elab_ctx ty)))
+      		 (* need to map to existing error categories*)
