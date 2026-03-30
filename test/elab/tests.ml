17c1c71541d348d4f529ea38b23adeb6e0dc0c94
hjy1 <forever56946@gmail.com>
2026-03-30 10:52:44 -0500

    apply finished

--- test/elab/tests.ml
+++ test/elab/tests.ml
@@ -6,8 +6,9 @@ let () =
       Test_pretty.suite;
       Test_kernel_pretty.suite;
       Test_nice_messages.suite;
+      Test_interface.suite;
       Test_tactic_refl.suite;
       Test_tactic_sorry.suite;
       Test_tactic_exact.suite;
-      Test_interface.suite;
+      Test_tactic_apply.suite;
     ]
