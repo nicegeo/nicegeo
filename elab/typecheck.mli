f726e80096fb2f5166d475ec07e88621a1b03b8e
Talia Ringer <talia@dependenttyp.es>
2026-04-08 12:16:47 -0500

    dune fmt mess

--- elab/typecheck.mli
+++ elab/typecheck.mli
@@ -21,7 +21,8 @@ val elaborate : Types.ctx -> term -> term option -> term
     be elaborated first. *)
 val infertype : ?depth:int -> Types.ctx -> term -> term
 
-(** [replace_metas ctx tm] replaces the metavariables in [tm] with their solved instances. *)
+(** [replace_metas ctx tm] replaces the metavariables in [tm] with their solved instances.
+*)
 val replace_metas : Types.ctx -> term -> term
 
 val unify :
