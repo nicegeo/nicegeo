73c669d73e7273ff2409b770072fcf98d221a78f
Talia Ringer <talia@dependenttyp.es>
2026-04-08 10:38:00 -0500

    some basic tests, but kernel def eq fails, need to debug why it thinks there is still a hole

---
+++ test/elab/test_tactic_exists.ml
@@ -0,0 +1,96 @@
+open Elab.Tactics
+open Elab.Proofstate
+
+(*
+ * This is based heavily on the rewrite tactic tests, including by copying
+ * several of the utility functions. We should refactor this common 
+ * infrastructure at some point, but for now I want to make sure we can get
+ * this tactic out to the library team for use.
+ *)
+
+let path_to_env = "../../../../synthetic/env.ncg"
+
+(*
+  Create the starting environment that we use in the tests.
+  It adds axioms for the types we care about in the tests.
+*)
+let make_env () =
+  let env = Elab.Interface.create_with_env_path path_to_env in
+  let process s =
+    let lexbuf = Lexing.from_string s in
+    let stmts = Elab.Parser.main Elab.Lexer.token lexbuf in
+    List.iter (Elab.Interface.process_statement env) stmts
+  in
+  process "Axiom A : Type";
+  process "Axiom a : A";
+  (env, process)
+
+let elab env s = Elab.Typecheck.elaborate env (Elab.Interface.parse_term s) None
+
+let run_tactic tac st =
+  match tac st with
+  | Failure msg -> Alcotest.failf "tactic failed: %s" msg
+  | Success st' -> st'
+
+(** Convert an elab term to a kernel term *)
+let to_kterm env tm =
+  Elab.Reduce.delta_reduce env tm true
+  |> Elab.Reduce.reduce env |> Elab.Convert.conv_to_kterm
+
+(** Check that the kernel accepts [proof] as having type [goal_ty]. *)
+let kernel_check env proof goal_ty =
+  let proof_k = to_kterm env (apply_meta env proof) in
+  let ty_k = to_kterm env goal_ty in
+  let inferred = Kernel.Infer.inferType env.kenv (Hashtbl.create 0) proof_k in
+  Kernel.Infer.isDefEq env.kenv (Hashtbl.create 0) inferred ty_k
+
+(* Check that single usage of `exists` creates the correct new goal type. *)
+let test_exists_simple () =
+  let env, _ = make_env () in
+  let goal_ty = elab env "Exists A (fun (a : A) => True)" in
+  let st = init_state ~elab_ctx:env goal_ty in
+  let st = run_tactic (exists (elab env "a")) st in
+  Alcotest.(check int) "one remaining goal" 1 (List.length st.open_goals);
+  let got = pp_term env (Elab.Reduce.reduce env (List.hd st.open_goals).goal_type) in
+  let exp = pp_term env (elab env "True") in
+  Alcotest.(check string) "new goal is True" exp got
+
+(*
+(* Check that `rewrite` fails when the `lhs` does not appear. *)
+let test_rewrite_no_match () =
+  let env, process = make_env () in
+  process "Axiom x : A";
+  process "Axiom y : A";
+  process "Axiom c : A";
+  process "Axiom d : A";
+  process "Axiom c_eq_d : Eq A c d";
+  let st = init_state ~elab_ctx:env (elab env "Eq A x y") in
+  match rewrite (elab env "c_eq_d") st with
+  | Success _ -> Alcotest.fail "expected missing lhs to fail"
+  | Failure _ -> ()*)
+
+(*
+  Checks that using `exists` and then closing a trivial goal produces
+  a proof term that the kernel accepts.
+*)
+let test_exists_kernel_check () =
+  let env, _ = make_env () in
+  let goal_ty = elab env "Exists A (fun (a : A) => True)" in
+  let st = init_state ~elab_ctx:env goal_ty in
+  let st = run_tactic (exists (elab env "a")) st in
+  let st = run_tactic (exact (elab env "True.intro")) st in
+  Alcotest.(check bool) "no remaining goals" true (is_complete st);
+  Alcotest.(check bool)
+    "kernel accepts proof"
+    true
+    (kernel_check env st.statement goal_ty)
+
+let suite =
+  let open Alcotest in
+  ( "Tactic.exists",
+    [
+      test_case "exists simple" `Quick test_exists_simple;
+     (* test_case "exists unify" `Quick test_exists_unify;
+      test_case "exists invalid goal" `Quick test_exists_invalid_goal;*)
+        test_case "exists kernel check" `Quick test_exists_kernel_check;
+    ] )
