aaef9187a984cbffedea96f44f734bee81a1190e
char3210 <45241413+char3210@users.noreply.github.com>
2026-04-26 21:40:37 -0500

    add tests no way it works

--- test/automation/test_fm_elim.ml
+++ test/automation/test_fm_elim.ml
@@ -13,6 +13,12 @@ let summand : summand Alcotest.testable =
         (Elab.Pretty.term_to_string env (Simpterm.from_simpterm (summand_to_term t))))
     (fun t1 t2 -> t1 = t2)
 
+let simpterm : Simpterm.term Alcotest.testable =
+  Alcotest.testable
+    (fun fmt t ->
+      Format.fprintf fmt "%s" (Elab.Pretty.term_to_string env (Simpterm.from_simpterm t)))
+    (fun t1 t2 -> t1 = t2)
+
 let check f =
   try f ()
   with Elab.Error.ElabError info ->
@@ -93,6 +99,75 @@ let test_to_constrain_basic () =
 
   ()
 
+let test_elim_eq () =
+  let open Simpterm in
+  let a, b, c, d = (Bvar 1, Bvar 2, Bvar 3, Bvar 4) in
+  let add t1 t2 = App (App (Name "Add", t1), t2) in
+  let ( ++ ) = add in
+  let eq t1 t2 = App (App (App (Name "Eq", Name "Measure"), t1), t2) in
+  let ( == ) = eq in
+  let lt t1 t2 = App (App (Name "Lt", t1), t2) in
+  let ( << ) = lt in
+  let constrain_terms =
+    [ (Bvar 5, a == b ++ c); (Bvar 6, a << d); (Bvar 7, b << a); (Bvar 8, c == d) ]
+  in
+  let constrains =
+    List.map
+      (fun (proof, tm) ->
+        let c = create_constrain tm proof in
+        match c with Some c -> c | None -> Alcotest.fail "create_constrain failed")
+      constrain_terms
+  in
+
+  let cs = elim_eq constrains 0 (Bvar 1) in
+  Alcotest.(check int) "number of remaining constrains" 3 (List.length cs);
+  let c1 = List.nth cs 0 in
+  let c2 = List.nth cs 1 in
+  let c3 = List.nth cs 2 in
+  Alcotest.(check simpterm) "c1 atom is replaced" (b ++ c << d) (constrain_ty c1);
+  Alcotest.(check simpterm) "c2 atom is replaced" (b << b ++ c) (constrain_ty c2);
+  Alcotest.(check simpterm) "c3 unchanged" (c == d) (constrain_ty c3);
+
+  let lctx =
+    List.map
+      (fun (proof, ty) ->
+        Elab.Types.
+          {
+            bid =
+              (match proof with Bvar bid -> bid | _ -> failwith "unexpected proof term");
+            name = None;
+            ty = from_simpterm ty;
+          })
+      constrain_terms
+    @ List.init 4 (fun i ->
+        Elab.Types.
+          {
+            bid = i + 1;
+            name = Some ("x" ^ string_of_int (i + 1));
+            ty = Elab.Proofstate.mk_name "Measure";
+          })
+  in
+
+  List.iter
+    (fun c ->
+      check (fun () ->
+          let proof_ty = Elab.Typecheck.infertype env lctx (from_simpterm c.proof) in
+          print_endline ("proof type: " ^ Elab.Pretty.term_to_string env proof_ty);
+          Elab.Typecheck.unify
+            env
+            ~lctx
+            proof_ty
+            (Hashtbl.create 0)
+            (constrain_ty c |> from_simpterm)
+            (Hashtbl.create 0)))
+    cs;
+
+  ()
+
 let suite =
   let open Alcotest in
-  ("fm_elim", [ test_case "to_constrain basic" `Quick test_to_constrain_basic ])
+  ( "fm_elim",
+    [
+      test_case "to_constrain basic" `Quick test_to_constrain_basic;
+      test_case "elim_eq" `Quick test_elim_eq;
+    ] )
